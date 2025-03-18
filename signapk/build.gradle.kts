85682fb73c6005aa2744cc26f5b033818bfdb919
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-03-18 10:54:46 +0100

    Initial commit
    Signed-off-by: Salvo Giangreco <giangrecosalvo9@gmail.com>

---
+++ signapk/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    alias(libs.plugins.shadow)
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.apksig)
+    implementation(libs.bcprov)
+    implementation(libs.bcpkix)
+    implementation(libs.conscrypt)
+}
+
+application {
+    mainClass.set("com.android.signapk.SignApk")
+}
+
+tasks.shadowJar {
+    minimize()
+}
